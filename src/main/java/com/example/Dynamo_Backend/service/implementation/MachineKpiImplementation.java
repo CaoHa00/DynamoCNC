2e60cf68328fb9b4f658fe000dc7c418f3aea04b
VyHoang245 <155942335+VyHoang245@users.noreply.github.com>
2025-08-06 11:14:40 +0700

    #37-upload-file-excel

--- src/main/java/com/example/Dynamo_Backend/service/implementation/MachineKpiImplementation.java
+++ src/main/java/com/example/Dynamo_Backend/service/implementation/MachineKpiImplementation.java
@@ -1,14 +1,19 @@
 package com.example.Dynamo_Backend.service.implementation;
 
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.Dynamo_Backend.dto.MachineKpiDto;
 import com.example.Dynamo_Backend.entities.Machine;
 import com.example.Dynamo_Backend.entities.MachineKpi;
+import com.example.Dynamo_Backend.entities.Staff;
 import com.example.Dynamo_Backend.mapper.MachineKpiMapper;
 import com.example.Dynamo_Backend.repository.MachineKpiRepository;
 import com.example.Dynamo_Backend.repository.MachineRepository;
@@ -92,6 +97,32 @@ public class MachineKpiImplementation implements MachineKpiService {
         machineKpi.setUpdatedDate(updatedTimestamp);
         MachineKpi saveMachineKpi = machineKpiRepository.save(machineKpi);
         return MachineKpiMapper.mapToMachineKpiDto(saveMachineKpi);
+    }
+
+    @Override
+    public void importMachineKpiFromExcel(MultipartFile file) {
+        try {
+            InputStream inputStream = file.getInputStream();
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                if (row.getRowNum() == 0)
+                    continue; // Skip header row
+                MachineKpiDto machineKpiDto = new MachineKpiDto();
+                machineKpiDto.setYear((int) row.getCell(0).getNumericCellValue());
+                machineKpiDto.setMonth((int) row.getCell(1).getNumericCellValue());
+                String idCell = row.getCell(0).getStringCellValue();
+                String machineId = idCell.substring(idCell.length() - 3, idCell.length() - 1);
+                machineKpiDto.setMachineId(Integer.parseInt(machineId));
+                machineKpiDto.setMachineMiningTarget((int) row.getCell(4).getNumericCellValue());
+                machineKpiDto.setOee((float) row.getCell(5).getNumericCellValue());
+                addMachineKpi(machineKpiDto);
+            }
+            workbook.close();
+            inputStream.close();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to import machine KPIs from Excel file", e);
+        }
     }
 
 }
